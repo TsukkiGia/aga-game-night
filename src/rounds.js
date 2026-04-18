8abb2b9965cf93c4d89aafa5ce8f99546a09b30c
Gianna Torpey <gianna.torpey@gmail.com>
2026-04-18 13:38:04 -0400

    Flags and Capitals

--- src/rounds.js
+++ src/rounds.js
@@ -1,10 +1,22 @@
 import charades        from './rounds/charades.js'
+import capitalCities from './rounds/capitalCities.js'
 import guessTheLanguage from './rounds/guessTheLanguage.js'
 import guessTheSong    from './rounds/guessTheSong.js'
+import flagTriviaDescriptions from './rounds/flagTriviaDescriptions.js'
+import flagsByImage from './rounds/flagsByImage.js'
 import slangBee         from './rounds/slangBee.js'
 import thesisTranslator from './rounds/thesisTranslator.js'
 
-const rounds = [guessTheLanguage, charades, slangBee, thesisTranslator, guessTheSong]
+const rounds = [
+  guessTheLanguage,
+  charades,
+  slangBee,
+  thesisTranslator,
+  guessTheSong,
+  capitalCities,
+  flagsByImage,
+  flagTriviaDescriptions,
+]
 
 export default rounds.map((r, i) => ({
   ...r,
